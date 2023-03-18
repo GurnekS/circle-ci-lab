54768050cde726190af766d65802cf671d3c459d
GurnekS <dinoman556@gmail.com>
2023-03-17 21:58:41 -0700

    dice test files yaml file added 12 sided die

--- src/test/java/DiceTest.java
+++ src/test/java/DiceTest.java
@@ -14,37 +14,37 @@ public class DiceTest {
     @Test
     public void testDice()
     {
-        Dice dice = new Dice(6, "blue");
-        assertEquals(6,dice.getSides());
+        Dice dice = new Dice(12, "blue");
+        assertEquals(12,dice.getSides());
         assertEquals("blue",dice.getColor());
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {1,2,3,4,5,6})
+    @ValueSource(ints = {1,2,3,4,5,6,7,8,9,10,11,12})
     public void testValidNumber(int number)
     {
-        Dice dice = new Dice(6, "blue");
+        Dice dice = new Dice(12, "blue");
 
         int roll = dice.roll();
-        assertTrue(roll >= 1 && roll <= 6);
+        assertTrue(roll >= 1 && roll <= 12);
 
     }
 //    code cov
     @Test
     public void testColors(){
-        Dice dice = new Dice(6, "green");
+        Dice dice = new Dice(12, "green");
         assertEquals("green", dice.getColor());
     }
 
     @Test
     public void testRoll()
     {
-        Dice dice = new Dice(6, "red");
+        Dice dice = new Dice(12, "red");
         int frequency = 1;
         int[] totalFrequencyOfRoll = new int[frequency];
         for (int i = 0; i < frequency ; i++) {
             totalFrequencyOfRoll[i] = dice.roll();
-            assertTrue(totalFrequencyOfRoll[i] >= 1 && totalFrequencyOfRoll[i] <=6);
+            assertTrue(totalFrequencyOfRoll[i] >= 1 && totalFrequencyOfRoll[i] <=12);
         }
 
 
@@ -53,8 +53,8 @@ public class DiceTest {
 
     @Test
     public void testToString(){
-        Dice dice = new Dice(6, "red");
-        assertEquals("A 6 sided die", dice.toString());
+        Dice dice = new Dice(12, "red");
+        assertEquals("A 12 sided die", dice.toString());
     }
 
 
